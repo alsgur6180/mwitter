28cec08f7e9e7031d248fb9b9f6348d7b46ecdc4
alsgur6180 <58066921+alsgur6180@users.noreply.github.com>
2021-02-02 08:18:40 +0900

    Activate delte button and edit button

--- src/components/Mweet.js
+++ src/components/Mweet.js
@@ -1,15 +1,56 @@
-import React from "react";
+import { dbService } from "myBase";
+import React, { useState } from "react";
 
-const Mweet = ({ mweetObj, isOwner }) => (
-  <div>
-    <h4>{mweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete button</button>
-        <button>Edit button</button>
-      </>
-    )}
-  </div>
-);
+const Mweet = ({ mweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newMweet, setNewMweet] = useState(mweetObj.text);
+  const onDeleteClick = () => {
+    const ok = window.confirm("Are you sure you want to delete this mweet?");
+    if (ok) {
+      dbService.doc(`mweets/${mweetObj.id}`).delete();
+    }
+  };
+  const toggleMweet = () => setEditing((prev) => !prev);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewMweet(value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dbService.doc(`mweets/${mweetObj.id}`).update({ text: newMweet });
+    setEditing(false);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              onChange={onChange}
+              type="text"
+              placeholder="edit your mweet"
+              value={newMweet}
+              required
+            ></input>
+            <input type="submit" value="Edit Mweet" />
+          </form>
+          <button onClick={toggleMweet}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{mweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete button</button>
+              <button onClick={toggleMweet}>Edit button</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Mweet;
