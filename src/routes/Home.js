9931b14416bc8badca5563c4a3bcf2f142810ce0
alsgur6180 <58066921+alsgur6180@users.noreply.github.com>
2021-02-04 10:29:27 +0900

    clean js files

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import Mweet from "components/Mweet";
-import { dbService, storageService } from "myBase";
+import { dbService } from "myBase";
 import React, { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import MweetFactory from "components/MweetFactory";
 
 const Home = ({ userObj }) => {
-  const [mweet, setMweet] = useState("");
   const [mweets, setMweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
   useEffect(() => {
     dbService.collection("mweets").onSnapshot((snapshot) => {
       const mweetArray = snapshot.docs.map((doc) => ({
@@ -16,66 +14,10 @@ const Home = ({ userObj }) => {
       setMweets(mweetArray);
     });
   }, []);
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = storageService
-        .ref()
-        .child(`${userObj.uid}/${uuidv4()}`);
-      const response = await attachmentRef.putString(attachment, "data_url");
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-    const mweetObj = {
-      text: mweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    };
-    await dbService.collection("mweets").add(mweetObj);
-    setMweet("");
-    setAttachment("");
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setMweet(value);
-  };
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-  const onClearAttachment = () => setAttachment(null);
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={mweet}
-          onChange={onChange}
-          type="text"
-          placeholder="What is your mind?"
-          maxLength={120}
-        />
-        <input type="file" accept="image/*" onChange={onFileChange}></input>
-        <input type="submit" value="Mweet" />
-        {attachment && (
-          <div>
-            <img src={attachment} alt={attachment} width="50px" height="50px" />
-            <button onClick={onClearAttachment}>Clear</button>
-          </div>
-        )}
-      </form>
+      <MweetFactory userObj={userObj} />
       <div>
         {mweets.map((mweet) => (
           <Mweet
